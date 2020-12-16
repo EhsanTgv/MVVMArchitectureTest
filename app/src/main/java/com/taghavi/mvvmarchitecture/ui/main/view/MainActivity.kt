dc6a316f702ca2f14cc922ea364ff881e405b5f4
EhsanTgv <EhsanTgv@gmail.com>
2020-12-16 16:02:22 +0330

    update activity_main design

--- app/src/main/java/com/taghavi/mvvmarchitecture/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/taghavi/mvvmarchitecture/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.taghavi.mvvmarchitecture
+package com.taghavi.mvvmarchitecture.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taghavi.mvvmarchitecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
